10e8ebde0e89cd877489d4f3c1d812d7c75d5755
YoorimChoi <cyl578057@gmail.com>
2024-10-24 17:37:51 +0900

    [feat] 위스키 요청 관련 관리자 기능 추가
     - 위스키 요청에 대한 승낙, 거부 기능 추가
     - 승낙 과정
       1.  uploads 에 있던 이미지 파일을 images로 옮기기
       2. 위스키 정보에 요청 정보 추가
       3. 요청에 생성된 위스키 외래키로 추가
     - 위 과정 관련하여 Reqeust 와 User 정보를 조합 필요 > DTO 추가
     - 위스키 정보 추가 과정 수정 및 모든 Request Get API 추가

--- Whiskey_TastingTale_Backend/Data/Repositories/WhiskeyRepository.cs
+++ Whiskey_TastingTale_Backend/Data/Repositories/WhiskeyRepository.cs
@@ -3,6 +3,7 @@ using Whiskey_TastingTale_Backend.Data.Entities;
 using Whiskey_TastingTale_Backend.Data.Context;
 using Whiskey_TastingTale_Backend.API.DTOs;
 using Azure;
+using System.Text.RegularExpressions;
 
 namespace Whiskey_TastingTale_Backend.Data.Repository
 {
@@ -18,15 +19,53 @@ namespace Whiskey_TastingTale_Backend.Data.Repository
 
         internal async Task<Whiskey> AddWhiskey(Whiskey whiskey)
         {
+            var uploadFileName = whiskey.img_index;
+
+            var uploadsFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+            var imagesFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+
+            var sourceFilePath = Path.Combine(uploadsFolderPath, uploadFileName);
+
+
+            if (!Directory.Exists(imagesFolderPath))
+            {
+                Directory.CreateDirectory(imagesFolderPath);
+            }
+
+            var files = Directory.GetFiles(imagesFolderPath)
+                .Select(Path.GetFileNameWithoutExtension)  // 파일명에서 확장자를 제거
+                .Where(f => Regex.IsMatch(f, @"^\d+$"))    // 숫자로만 이루어진 파일 필터링
+                .Select(f => int.Parse(f))                 // 숫자로 변환
+                .OrderByDescending(f => f)                 // 내림차순으로 정렬
+                .ToList();
+
+            int newFileNumber = (files.Any() ? files.First() : 0) + 1;
+            string extension = Path.GetExtension(uploadFileName);  // 원본 파일의 확장자 유지
+            string newFileName = $"{newFileNumber}{extension}";   // 새 파일명: 번호 + 확장자
+
+            var destinationFilePath = Path.Combine(imagesFolderPath, newFileName);
+
+            // uploads 폴더에서 파일이 존재하는지 확인
+            if (!System.IO.File.Exists(sourceFilePath))
+            {
+                throw new Exception("해당 이미지가 존재하지 않습니다."); 
+            }
+
+            System.IO.File.Move(sourceFilePath, destinationFilePath);
+
             var temp = new Whiskey()
             {
+                img_index = newFileName,
+                whiskey_name = whiskey.whiskey_name,
                 alcohol_degree = whiskey.alcohol_degree,
                 details = whiskey.details,
                 maker = whiskey.maker
-                //TODO) 이미지는 어떻게 넣지? img_index
-            };
+            }; 
             await _whiskeyContext.whiskeys.AddAsync(temp);
             await _whiskeyContext.SaveChangesAsync();
+
+            temp.rating = 0.0;
+            temp.review_count = 0; 
 
             return temp;
         }
